8d925f22a76544cbe99cf849d8188b0e5874b9d6
SHAUNDRA\shaun <shaundra.d.strong@gmail.com>
2026-03-04 21:37:59 -0500

    added ejs files

--- server.js
+++ server.js
@@ -5,26 +5,38 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const expressLayouts = require("express-ejs-layouts");
+
+/* *******************************************
+* View Engine and Templates
+******************************************** */
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"});
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on ${host}:${port}`);
 })
