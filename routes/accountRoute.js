7f335063cd1bd868762573d7839bf882cccfb6e5
SHAUNDRA\shaun <shaundra.d.strong@gmail.com>
2026-03-28 14:44:02 -0400

    html forms edited and validation- sanitize and validate activities

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/");
+const regValidate = require('../utilities/account-validation')
+
 
 //Route to buil login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -11,6 +13,13 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegistration));
 
 // Route to register account
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
+
+
 
 module.exports = router;
